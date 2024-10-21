94b84eefa193f479d6fdf10d8177460773a14ac2
Yumo <dragon.tyl@163.com>
2024-10-21 18:13:47 +0800

    修改配置参数的加载方式

--- config.py
+++ config.py
@@ -3,10 +3,27 @@
 # FILE_SERVER_IP = '172.33.38.226'
 # HIDDEN_PATH = '/data/work/html/outFile'
 
-MQ_SERVER_IP = '192.168.1.109'
-FILE_SERVER_IP = '192.168.1.109'
-HIDDEN_PATH = '/data1/work/html/outFile'
-MINIO_ACCESS_KEY = 'adminminio'
-MINIO_SECRET_KEY = 'adminminio'
-MINIO_SERVER_IP = '192.168.1.109'
-MINIO_PORT = 9000
+
+import yaml
+
+# 设置配置文件的路径
+CONFIG_FILE_PATH = '/usr/src/app/ocr_config.yml'
+
+config = {}
+
+# 尝试加载配置文件
+try:
+    with open(CONFIG_FILE_PATH, 'r') as file:
+        config = yaml.safe_load(file)
+        print(f"Configuration loaded from {CONFIG_FILE_PATH}")
+except FileNotFoundError:
+    print(f"Configuration file not found at {CONFIG_FILE_PATH}. Using default configuration.")
+
+# 从配置中读取参数，如果没有则使用默认值
+MQ_SERVER_IP = config.get('MQ_SERVER_IP', '127.0.0.1')
+FILE_SERVER_IP = config.get('FILE_SERVER_IP', '127.0.0.1')
+HIDDEN_PATH = config.get('HIDDEN_PATH', '/temp')
+MINIO_ACCESS_KEY = config.get('MINIO_ACCESS_KEY', 'minioadmin')
+MINIO_SECRET_KEY = config.get('MINIO_SECRET_KEY', 'minioadmin')
+MINIO_SERVER_IP = config.get('MINIO_SERVER_IP', '127.0.0.1')
+MINIO_PORT = config.get('MINIO_PORT', 9000)
